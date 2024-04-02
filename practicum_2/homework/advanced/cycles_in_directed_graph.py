f1e24d293d2a1cb4bc9924eeec6b5657547dcb3f
Vyacheslav Bek <slavabek.games@gmail.com>
2024-04-02 22:57:07 +0300

    homework_practicum_2

--- practicum_2/homework/advanced/cycles_in_directed_graph.py
+++ practicum_2/homework/advanced/cycles_in_directed_graph.py
@@ -1,23 +1,17 @@
 import networkx as nx
 
-
 TEST_GRAPH_FILES = [
     "graph_1_wo_cycles.edgelist",
     "graph_2_w_cycles.edgelist",
 ]
 
-
 def has_cycles(g: nx.DiGraph):
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
-
-    pass
-
+    cycles = list(nx.simple_cycles(g)) # nx.simple_cycles() ищет все простые циклы
+    return len(cycles) > 0
 
 if __name__ == "__main__":
     for filename in TEST_GRAPH_FILES:
         # Load the graph
-        G = nx.read_edgelist("practicum_2/homework/graph_2.edgelist", create_using=nx.DiGraph)
+        G = nx.read_edgelist("practicum_2/graph_2.edgelist", create_using=nx.DiGraph)
         # Output whether it has cycles
         print(f"Graph {filename} has cycles: {has_cycles(G)}")
--- practicum_2/homework/basic/cycles_in_undirected_graph.py
+++ practicum_2/homework/basic/cycles_in_undirected_graph.py
@@ -1,23 +1,17 @@
 import networkx as nx
 
-
 TEST_GRAPH_FILES = [
     "graph_1_wo_cycles.edgelist",
     "graph_2_w_cycles.edgelist",
 ]
 
-
 def has_cycles(g: nx.Graph):
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
-
-    pass
-
+    cycles = list(nx.simple_cycles(g))
+    return len(cycles) > 0
 
 if __name__ == "__main__":
     for filename in TEST_GRAPH_FILES:
         # Load the graph
-        G = nx.read_edgelist("practicum_2/homework/graph_2.edgelist", create_using=nx.Graph)
+        G = nx.read_edgelist("practicum_2/graph_2.edgelist", create_using=nx.Graph)
         # Output whether it has cycles
         print(f"Graph {filename} has cycles: {has_cycles(G)}")
