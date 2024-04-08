2cd791ad1a4d70062907cc41db84cedd6276b160
Никита <nikitopus05@mail.ru>
2024-04-08 15:03:48 +0300

    Classwork 06.04

--- practicum_3/mst.py
+++ practicum_3/mst.py
@@ -3,7 +3,7 @@ from typing import Any
 import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
-from networkx.algorithms import tree
+
 from src.plotting import plot_graph
 
 
@@ -11,6 +11,7 @@ def prim_mst(G: nx.Graph, start_node="0") -> set[tuple[Any, Any]]:
     mst_set = set()  # set of nodes included into MST
     rest_set = set(G.nodes())  # set of nodes not yet included into MST
     mst_edges = set()  # set of edges constituting MST
+
     mst_set.add(start_node)
     rest_set.remove(start_node)
     while rest_set:
@@ -33,10 +34,8 @@ def prim_mst(G: nx.Graph, start_node="0") -> set[tuple[Any, Any]]:
     return mst_edges
 
 
-
 if __name__ == "__main__":
     G = nx.read_edgelist("practicum_3/graph_1.edgelist", create_using=nx.Graph)
-    plot_graph(G)
     mst_edges = prim_mst(G, start_node="0")
     plot_graph(G, highlighted_edges=list(mst_edges))
     print()
