b74f5d3d468d058829144079a5c6d4a3e518c286
DarIaSed <149616900+DarIaSed@users.noreply.github.com>
2024-05-03 23:17:45 +0500

    made homework6

---
+++ practicum_5/cholesky.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def cholesky(A):
+    n = len(A)
+    L = np.zeros_like(A)
+
+    for i in range(n):
+        for j in range(i + 1):
+            s = sum(L[i][k] * L[j][k] for k in range(j))
+            if i == j:
+                L[i][j] = np.sqrt(A[i][i] - s)
+            else:
+                L[i][j] = (1.0 / L[j][j] * (A[i][j] - s))
+    return L
+
+if __name__ == "__main__":
+    L = np.array(
+        [
+            [1.0, 0.0, 0.0],
+            [4.0, 2.0, 0.0],
+            [6.0, 5.0, 3.0],
+        ]
+    )
+    A = L @ L.T
+    L = cholesky(A)
+    print(L)
