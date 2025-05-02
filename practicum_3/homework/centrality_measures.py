876895e4c365eb7393c2487d78c19f5a7712b9fe
Ilya Osipov <adioshuesos@gmail.com>
2025-05-02 17:31:17 +0300

    add project root export to PATH in code

--- practicum_3/homework/centrality_measures.py
+++ practicum_3/homework/centrality_measures.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+project_root = Path(__file__).resolve().parents[2]
+sys.path.insert(0, str(project_root))
+
 from typing import Any, Protocol
 from itertools import combinations
 from collections import deque
