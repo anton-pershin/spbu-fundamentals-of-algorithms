af1ff96417718bb1df09615f9baa061152614eb1
Skm-c <skam2006maks@gmail.com>
2025-05-29 19:54:13 +0300

    Update huffman.py

--- practicum_5/homework/huffman.py
+++ practicum_5/homework/huffman.py
@@ -20,9 +20,10 @@ class HuffmanCoding:
         self.in_code = []
 
     def encode(self, sequence: list[Any]) -> str:
+
         self.freq = {el: 0 for el in sequence}
-        self.res = {el: '' for el in sequence}
-        self.decode_res = {el: '' for el in sequence}
+        self.res = {str(el): '' for el in sequence}
+        self.decode_res = {str(el): '' for el in sequence}
 
         for el in sequence:
             self.freq[el] += 1
@@ -35,73 +36,96 @@ class HuffmanCoding:
             min2 = min(self.freq, key=self.freq.get)
             del self.freq[min2]
 
-            for el_of_min1 in min1:
-                self.res[el_of_min1] = '0' + self.res[el_of_min1]
-            for el_of_min2 in min2:
-                self.res[el_of_min2] = '1' + self.res[el_of_min2]
+            min1 = str(min1)
+            min2 = str(min2)
+            i = 0
+            s = ''
+            while i < len(min1):
+                if min1[i] == '_':
+                    if s in self.res:
+                        self.res[s] = '0' + self.res[s]
+                    else:
+                        self.res[s] = '0'
+                    s = ''
+                else:
+                    s += min1[i]
+                i += 1
+            if s in self.res:
+                self.res[s] = '0' + self.res[s]
+            else:
+                self.res[s] = '0'
 
-            self.freq[min1 + min2] = min_v1 + min_v2
+            i = 0
+            s = ''
+            while i < len(min2):
+                if min2[i] == '_':
+                    if s in self.res:
+                        self.res[s] = '1' + self.res[s]
+                    else:
+                        self.res[s] = '1'
+                    s = ''
+                else:
+                    s += min2[i]
+                i += 1
+            if s in self.res:
+                self.res[s] = '1' + self.res[s]
+            else:
+                self.res[s] = '1'
+
+            self.freq[min1 + '_' + min2] = min_v1 + min_v2
 
         code = ''
         for el in sequence:
+            el = str(el)
             code += self.res[el]
             self.in_code.append(self.res[el])
             self.decode_res[self.res[el]] = el
+
         return code
 
     def decode(self, encoded_sequence: str) -> list[Any]:
         s = ''
         decode = []
+
         for el in encoded_sequence:
             s += el
             if s in self.in_code:
                 decode.append(self.decode_res[s])
                 s = ''
+
         return decode
 
 
 class LossyCompression:
     def __init__(self) -> None:
-
-        self.intervals = []
-        self.centers = []
+        self.huffman = HuffmanCoding()
         self.code = None
+        self.min = None
+        self.max = None
 
     def compress(self, time_series: NDArrayFloat) -> str:
-
-        self.len = len(time_series)
         self.min = np.min(time_series)
         self.max = np.max(time_series)
-        self.interval_len = (max(time_series) - min(time_series)) / len(time_series)
-        self.intervals = [np.min(time_series)]
 
-        for k in range(self.len):
-            self.intervals.append(self.intervals[-1] + self.interval_len)
-            self.centers.append((self.intervals[-2] + self.intervals[-1]) / 2)
-        self.code = np.digitize(time_series, bins=self.intervals) - 1
-        code = '_'.join(map(str, self.code))
-        return code
+        quantized = [
+            int(((x - self.min) / (self.max - self.min)) * (2 ** 4 - 1))
+            for x in time_series
+        ]
+
+        return self.huffman.encode(quantized)
 
     def decompress(self, bits: str) -> NDArrayFloat:
-        i = 0
-        s = ''
-        ind = []
-        while i < len(bits):
-            if bits[i] != '_':
-                s += bits[i]
-            else:
-                ind.append(int(s))
-                s = ''
-            i += 1
-        ind.append(int(s))
-        res = [self.centers[i] for i in ind]
-        return np.array(res, dtype=np.float64)
+        quantized = np.array(self.huffman.decode(bits), dtype=np.float32)
+        normalized = quantized / (2 ** 4 - 1)
+
+        return normalized * (self.max - self.min) + self.min
 
 if __name__ == "__main__":
     ts1 = np.loadtxt("ts_homework_practicum_5.txt")
 
     compressor = LossyCompression()
     bits = compressor.compress(ts1)
+
     decompressed_ts = compressor.decompress(bits)
 
     compression_ratio = (len(ts1) * 32 * 8) / len(bits)
