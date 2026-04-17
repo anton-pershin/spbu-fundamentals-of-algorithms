4cf465029e243c8333a345b1d35547f8122badc8
Tanya <tagilcevatatana20@gmail.com>
2026-04-17 20:42:11 +0300

    practicum_5

--- practicum_5/homework/huffman.py
+++ practicum_5/homework/huffman.py
@@ -9,58 +9,100 @@ import numpy as np
 from src.plotting.graphs import plot_graph
 from src.common import AnyNxGraph, NDArrayFloat
 
+class Node:
+    def __init__(self, char=None, frequency=None):
+        self.char = char
+        self.frequency = frequency
+        self.left = None
+        self.right = None
+
+    def __lt__(self, other):
+        return self.frequency < other.frequency
+
 
 class HuffmanCoding:
     def __init__(self) -> None:
-
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
-        pass
+        self.codes = {}
+        self.decode_map = {}
+        self.b  = None
 
     def encode(self, sequence: list[Any]) -> str:
+        freq = {}
+        for c in sequence:
+            if c in freq:
+                freq[c]+=1
+            else:
+                freq[c] = 1
+        priority_queue = [Node(char, f) for char, f in freq.items()]
+        heapq.heapify(priority_queue)
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
+        while len(priority_queue) > 1:
+            left = heapq.heappop(priority_queue)
+            right = heapq.heappop(priority_queue)
 
-        pass
-        
+            merged = Node(frequency=left.frequency + right.frequency)
+            merged.left = left
+            merged.right = right
+
+            heapq.heappush(priority_queue, merged)
+
+        root = priority_queue[0]
+        def build(node, code=""):
+            if node.char is not None:
+                self.codes[node.char] = code
+                self.decode_map[code] = node.char
+                return
+
+            build(node.left, code + "0")
+            build(node.right, code + "1")
+        build(root)
+        return "".join(self.codes[s] for s in sequence)
+                        
 
     def decode(self, encoded_sequence: str) -> list[Any]:
+        decoded = []
+        cur_code = ""
+        for bit in encoded_sequence:
+            cur_code+=bit
+            if cur_code in self.decode_map:
+                decoded.append(self.decode_map[cur_code])
+                cur_code = ""
+        return decoded
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
-        pass
 
 
 class LossyCompression:
     def __init__(self) -> None:
-
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
-        pass
+        self.K = 32
+        self.b = None
+        self.centres = None
+        self.huffman = HuffmanCoding()
 
     def compress(self, time_series: NDArrayFloat) -> str:
+        xmin = np.min(time_series)
+        xmax = np.max(time_series)
+        gran = np.linspace(xmin,xmax, self.K + 1)
+        self.centres= (gran[:-1] + gran[1:])/2
+        self.step = (xmax - xmin)/ self.K
+        quantized = []
+        for x in time_series:
+            i = int((x - xmin) / self.step)
+            if i < 0:
+                i = 0
+            elif i >= self.K:
+                i = self.K - 1
+            quantized.append(i)
+        c= self.huffman.encode(quantized)
+        return c
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
 
-        pass
 
     def decompress(self, bits: str) -> NDArrayFloat:
-
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
-        pass
+        indx= self.huffman.decode(bits)
+        decompressed = []
+        for i in indx:
+            decompressed.append(self.centres[i])
+        return np.array(decompressed)
 
 
 if __name__ == "__main__":
