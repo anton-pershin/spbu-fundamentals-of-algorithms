17d05f1c67d6165618339e374dd09a7e68fc881e
Mo1ecu1a <puazhue@gmail.com>
2026-04-18 10:50:40 +0300

    practicum 6 classwork

--- practicum_6/dp_for_shortest_path.py
+++ practicum_6/dp_for_shortest_path.py
@@ -4,6 +4,7 @@ from typing import Any
 import networkx as nx
 import numpy as np
 
+from practicum_4.dfs import TopologicalSorting
 from src.plotting.graphs import plot_graph
 from src.common import AnyNxGraph
 
@@ -13,20 +14,31 @@ class DpAlgorithmForShortestPath:
     Shortest path algorithm for directed acyclic graphs.
     """ 
     def __init__(self, G: nx.DiGraph) -> None:
+        self.G: nx.DiGraph = G
+        self.topo_sorting = TopologicalSorting(G)
+        self.dist: dict[Any, int] = {}
+        self.shortest_paths: dict[Any, set[tuple[Any, Any]]] = {}
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
-        pass
 
     def run(self, node: Any) -> None:
+        shorted_nodes = self.topo_sorting.sort(node)
+        self.dist[node] = 0
+        self.shortest_paths[node] = set()
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
+        for cur_node in shorted_nodes[1:]:
+            predessesor_node = None
+            min_path_weight = np.inf
 
-        pass
+            for n_neigh in self.G.predecessors(cur_node):
+                path_weight = self.dist[n_neigh] + self.G.edges[n_neigh, cur_node]["weight"]
+                if path_weight < min_path_weight:
+                    min_path_weight = path_weight
+                    predessesor_node = n_neigh
+            
+            self.dist[cur_node] = min_path_weight
+            self.shortest_paths[cur_node] = self.shortest_paths[predessesor_node] | {(predessesor_node, cur_node)}
+        
+            
 
 
 class DpAlgorithmForShortestReliablePath:
@@ -34,7 +46,7 @@ class DpAlgorithmForShortestReliablePath:
     Shortest path algorithm for directed acyclic graphs with additional
     constraint: the path cannot contain more than k edges
     """ 
-    def __init__(self, G: nx.DiGraph) -> None:
+    def __init__(self, G: nx.DiGraph, k: int) -> None:
 
         ##########################
         ### PUT YOUR CODE HERE ###
