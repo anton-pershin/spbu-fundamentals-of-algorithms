6a6ea4b1d7657a6ad4c79c0dc0be2cde6f7610c3
dariabeaufort <zdavydova10@gmail.com>
2026-04-27 11:29:01 +0300

    Added solution for homework practicum 6

--- practicum_6/homework/matrix_chain_multiplication.py
+++ practicum_6/homework/matrix_chain_multiplication.py
@@ -10,23 +10,70 @@ from src.common import AnyNxGraph
 
 class MatrixChainMultiplication:
     def __init__(self) -> None:
-
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        #########################
-
-        pass
+        self.node_id = 0
+        self.dp = None
+        self.dims = None
+        self.split = None
 
     def run(
         self,
         matrices: list[dict[str, Union[str, tuple[int, int]]]]
     ) -> tuple[AnyNxGraph, Any]:
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        #########################
+        num = len(matrices)
 
-        pass
+        dimensions = [matrices[0]["shape"][0]]
+        for matrix in matrices:
+            dimensions.append(matrix["shape"][1])
+
+        self.dims = dimensions
+
+        min_cost = [[0] * num for _ in range(num)]
+        split = [[0] * num for _ in range(num)]
+
+        for length in range(2, num + 1):
+            for start in range(num - length + 1):
+                end = start + length - 1
+                min_cost[start][end] = float("inf")
+
+                for split_point in range(start, end):
+                    cost = (
+                        min_cost[start][split_point] + min_cost[split_point + 1][end]
+                        + dimensions[start] * dimensions[split_point + 1] * dimensions[end + 1]
+                    )
+
+                    if cost < min_cost[start][end]:
+                        min_cost[start][end] = cost
+                        split[start][end] = split_point
+
+        self.dp = min_cost
+        self.split = split
+
+        G = nx.Graph()
+
+        def build_tree(start, end):
+            if start == end:
+                name = matrices[start]["matrix_name"]
+                G.add_node(name, label=name)
+                return name
+
+            split_point = split[start][end]
+
+            left = build_tree(start, split_point)
+            right = build_tree(split_point + 1, end)
+
+            self.node_id += 1
+            operation_node = f"mul_{self.node_id}"
+            
+            G.add_node(operation_node, label="*")
+            G.add_edge(operation_node, left)
+            G.add_edge(operation_node, right)
+
+            return operation_node
+
+        root = build_tree(0, num - 1)
+
+        return G, root
 
 
 if __name__ == "__main__":
@@ -54,4 +101,3 @@ if __name__ == "__main__":
     matmul_tree, root = mcm.run(test_matrices)
 
     plot_graph(matmul_tree)
-
