dc2cba24b4b3884a255f43fb286c16b1d27f2f5c
m4rkrly <w1ltsh1rem4rk@gmail.com>
2026-04-30 21:09:47 +0300

    Made Matrix class

--- practicum_6/homework/matrix_chain_multiplication.py
+++ practicum_6/homework/matrix_chain_multiplication.py
@@ -7,26 +7,49 @@ import networkx as nx
 from src.plotting.graphs import plot_graph
 from src.common import AnyNxGraph
 
+class Matrix():
+    def __init__(
+            self,
+            prod: str,
+            shape: tuple[int, int],
+            complexity: int,
+            tree: list[tuple[str, str]]
+    ) -> None:
+        self.prod = prod
+        self.shape = shape
+        self.complexity = complexity
+        self.tree = tree 
+
+    def __eq__(self, other) -> bool:
+        if type(other) == str:
+            return self.name == other
+        return False
+
+    def __repr__(self) -> str:
+        text = (
+            f"\nProduction: {self.prod}\n"
+            f"Shape: {self.shape}\n"
+            f"Complexity: {self.complexity}\n"
+            f"Tree: {self.tree}"
+        )
+        return text
 
 class MatrixChainMultiplication:
     def __init__(self) -> None:
+        self.matrices: list[dict[str, str | tuple[int, int] | int]] = []
+        self.bestTree: list[tuple[str, str]] = []
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        #########################
-
-        pass
+    def preprocess(self, matrices):
+        for old in matrices:
+            new = Matrix(old["matrix_name"], old["shape"], 0, [])
+            self.matrices.append(new)
 
     def run(
         self,
         matrices: list[dict[str, Union[str, tuple[int, int]]]]
     ) -> tuple[AnyNxGraph, Any]:
-
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        #########################
-
-        pass
+        self.preprocess(matrices)
+        print(self.matrices)
 
 
 if __name__ == "__main__":
@@ -51,7 +74,8 @@ if __name__ == "__main__":
     ]
 
     mcm = MatrixChainMultiplication()
-    matmul_tree, root = mcm.run(test_matrices)
+    mcm.run(test_matrices)
+    #matmul_tree, root = mcm.run(test_matrices)
 
-    plot_graph(matmul_tree)
+    #plot_graph(matmul_tree)
 
