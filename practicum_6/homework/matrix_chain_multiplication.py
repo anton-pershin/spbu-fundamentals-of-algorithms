2411bd4a5559e2f66d57ae5238eb212a30b8bc07
Alyona-slukinova <slukinova.alyona@gmail.com>
2026-05-01 14:02:12 +0300

    homework

--- practicum_6/homework/matrix_chain_multiplication.py
+++ practicum_6/homework/matrix_chain_multiplication.py
@@ -10,24 +10,62 @@ from src.common import AnyNxGraph
 
 class MatrixChainMultiplication:
     def __init__(self) -> None:
+        self.dp = None
+        self.split = None
+        self.sizes = None
+    
+    def build(self, l, r, names):
+        if l == r - 1:
+            node = names[l]
+            self.G.add_node(node)
+            return node
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        #########################
+        i = self.split[l, r]
 
-        pass
+        left = self.build(l, i, names)
+        right = self.build(i, r, names)
+
+        self.node_num += 1
+        parent = str(self.node_num)
+        self.G.add_node(parent)
+
+        self.G.add_edge(parent, left)
+        self.G.add_edge(parent, right)
+
+        return parent
 
     def run(
         self,
         matrices: list[dict[str, Union[str, tuple[int, int]]]]
     ) -> tuple[AnyNxGraph, Any]:
+        n = len(matrices)
+        self.G = nx.Graph()
+        self.node_num = 0
+        if n == 0:
+            return self.G, None
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        #########################
+        names = [m["matrix_name"] for m in matrices]
+        shapes = [m["shape"] for m in matrices]
+        self.sizes = np.array([shapes[0][0]] + [s[1] for s in shapes], dtype=int)
 
-        pass
+        self.dp = np.full((n + 1, n + 1), np.inf)
+        self.split = np.full((n + 1, n + 1), -1, dtype=int)
+        
+        for i in range(n):
+            self.dp[i, i+1] = 0
 
+        for i in range(2, n + 1):
+            for l in range(0, n - i + 1):
+                r = l + i
+                for k in range(l + 1, r):
+                    curr = (self.dp[l, k] + self.dp[k, r] + self.sizes[l] * self.sizes[k] * self.sizes[r])
+
+                    if curr < self.dp[l, r]:
+                        self.dp[l, r] = curr
+                        self.split[l, r] = k
+
+        root = self.build(0, n, names)
+        return self.G, root
 
 if __name__ == "__main__":
 
