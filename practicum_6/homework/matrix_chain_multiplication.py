0f5ad2b3cd9ccdb7a8582b5044d6039744ee3c5a
Mo1ecu1a <puazhue@gmail.com>
2026-05-01 18:20:34 +0300

    B15_Yani_Polina_practicum_6

--- practicum_6/homework/matrix_chain_multiplication.py
+++ practicum_6/homework/matrix_chain_multiplication.py
@@ -10,23 +10,58 @@ from src.common import AnyNxGraph
 
 class MatrixChainMultiplication:
     def __init__(self) -> None:
+        self.count = 0
+        self.sizes = []
+        self.table_costs = []
+        self.table_splits = []
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        #########################
-
-        pass
 
     def run(
         self,
         matrices: list[dict[str, Union[str, tuple[int, int]]]]
     ) -> tuple[AnyNxGraph, Any]:
+        self.count = len(matrices)
+        self.sizes = []
+        self.sizes.append(matrices[0]["shape"][0])
+        for matrix in matrices:
+            self.sizes.append(matrix["shape"][1])
+        
+        self.table_costs = np.zeros((self.count, self.count))
+        self.table_splits = np.zeros((self.count, self.count)) 
+        
+        for length in range(2, self.count + 1):
+            for start in range(self.count - length + 1):
+                end = start + length - 1
+                self.table_costs[start][end] = float('inf')
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        #########################
+                for k in range(start, end):
+                    cost = (self.table_costs[start][k] + self.table_costs[k + 1][end] + self.sizes[start] * self.sizes[k + 1] * self.sizes[end + 1])
 
-        pass
+                    if cost < self.table_costs[start][end]:
+                        self.table_costs[start][end] = cost
+                        self.table_splits[start][end] = k
+        
+        graph = nx.Graph()
+        counter = [0]
+
+        def build(start, end):
+            if start == end:
+                return matrices[start]["matrix_name"]
+            
+            k = int(self.table_splits[start][end])
+            node_name = "node_" + str(counter[0])
+            counter[0] += 1
+            graph.add_node(node_name)
+
+            left = build(start, k)
+            right = build(k + 1, end)
+
+            graph.add_edge(node_name, left)
+            graph.add_edge(node_name, right)
+            return node_name
+        
+        root = build(0, self.count - 1)
+        return graph, root
 
 
 if __name__ == "__main__":
