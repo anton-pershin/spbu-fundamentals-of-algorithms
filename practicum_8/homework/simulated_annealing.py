faeff69b1283916f80aecc97039e7a8dbc344029
Arina-Alex <mamont.bk@mail.ru>
2026-05-10 13:46:51 +0300

    B16_Mamontova_Arina_practicum_8

--- practicum_8/homework/simulated_annealing.py
+++ practicum_8/homework/simulated_annealing.py
@@ -22,16 +22,52 @@ def set_colors(G: nx.Graph, colors: NDArrayInt) -> None:
     for n, color in zip(G.nodes, colors):
         G.nodes[n]["color"] = color
 
+def tweak(colors: NDArrayInt, n_max_colors: int) -> NDArrayInt:
+    new_colors = colors.copy()
+    n_nodes = len(new_colors)
+    random_i = np.random.randint(0, n_nodes)
+    random_color = np.random.randint(0, n_max_colors)
+    new_colors[random_i] = random_color
+    return new_colors
+
 
 def solve_via_simulated_annealing(
     G: nx.Graph, n_max_colors: int, initial_colors: NDArrayInt, n_iters: int
 ) -> NDArrayInt:
     loss_history = np.zeros((n_iters,), dtype=np.int_)
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    cur_colors = initial_colors
+    cur_conf = number_of_conflicts(G, cur_colors)
 
+    best_colors = cur_colors
+    best_conf = cur_conf
+    
+    loss_history[0] = best_conf
+    T = 1.0
+    cooling_rate = 0.95
+
+    for i in range(1, n_iters):
+        T = T * cooling_rate
+
+        new_colors = tweak(cur_colors, n_max_colors)
+        new_conf = number_of_conflicts(G, new_colors)
+    
+        delt = new_conf - cur_conf
+        if delt < 0:
+            deci = True
+        else:
+            chance = np.exp(-delt/T)
+            deci = chance > np.random.random() 
+
+        if deci:
+            cur_colors = new_colors
+            cur_conf = new_conf
+        
+            if cur_conf < best_conf:
+                best_colors = cur_colors
+                best_conf = cur_conf
+        
+        loss_history[i] = best_conf
     return loss_history
 
 
