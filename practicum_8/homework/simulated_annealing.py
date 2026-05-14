4576d628b7e608d72bfe94d11f1f3c5c0129ac0a
Pokachalova Viktoria <poka4alovavika@gmail.com>
2026-05-15 03:11:26 +0700

    B15_Pokachalova_Viktoria_practicum_8

--- practicum_8/homework/simulated_annealing.py
+++ practicum_8/homework/simulated_annealing.py
@@ -28,10 +28,27 @@ def solve_via_simulated_annealing(
 ) -> NDArrayInt:
     loss_history = np.zeros((n_iters,), dtype=np.int_)
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
-
+    cur_colors=initial_colors.copy()
+    cur_conflicts=number_of_conflicts(G,cur_colors)
+    loss_history[0]=cur_conflicts
+    temperature=10
+    alpha=0.99
+    for i in range(1,n_iters):
+        new_colors=cur_colors.copy()
+        v=np.random.randint(0, len(G.nodes))
+        color=np.random.randint(0,n_max_colors)
+        new_colors[v]=color
+        new_conflicts=number_of_conflicts(G,new_colors)
+        if new_conflicts<=cur_conflicts:
+            cur_conflicts=new_conflicts
+            cur_colors=new_colors
+        else:
+            r = np.random.random()
+            if r<np.exp((cur_conflicts-new_conflicts) / temperature):
+                cur_conflicts = new_conflicts
+                cur_colors = new_colors
+        loss_history[i] = cur_conflicts
+        temperature = temperature * alpha
     return loss_history
 
 
