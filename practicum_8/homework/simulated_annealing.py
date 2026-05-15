95c2624e3467a581a17b60dc4fbc00c833f429d0
Tanya <tagilcevatatana20@gmail.com>
2026-05-15 20:55:15 +0300

    practicum_8

--- practicum_8/homework/simulated_annealing.py
+++ practicum_8/homework/simulated_annealing.py
@@ -27,10 +27,30 @@ def solve_via_simulated_annealing(
     G: nx.Graph, n_max_colors: int, initial_colors: NDArrayInt, n_iters: int
 ) -> NDArrayInt:
     loss_history = np.zeros((n_iters,), dtype=np.int_)
+    T = 100
+    current_colors = initial_colors.copy()
+    current_loss = number_of_conflicts(G, current_colors)
+    for i in range(n_iters):
+        T*=0.95
+        new_colors = current_colors.copy()
+        n1 = np.random.randint(0, len(G.nodes))
+        n2 = np.random.randint(0, len(G.nodes))
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+        new_colors[n1] = np.random.randint(0, n_max_colors)
+        new_colors[n2] = np.random.randint(0, n_max_colors)
+
+        new_loss = number_of_conflicts(G, new_colors)
+        if new_loss <= current_loss:
+            current_colors = new_colors
+            current_loss = new_loss
+        else:
+            delta = new_loss - current_loss
+            probability = np.exp(-delta/T)
+            if np.random.rand() < probability:
+                current_colors = new_colors
+                current_loss = new_loss
+        loss_history[i] = current_loss
+    set_colors(G, current_colors) 
 
     return loss_history
 
