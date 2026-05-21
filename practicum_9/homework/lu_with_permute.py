2b91b3ff017c9b23133476f448783961bdb54069
Victoria Soboleva <vikhp1207@icloud.com>
2026-05-21 19:28:31 +0300

    B15_Soboleva_Viktoria_practicum_9

--- practicum_9/homework/lu_with_permute.py
+++ practicum_9/homework/lu_with_permute.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 import numpy as np
 from numpy.typing import DTypeLike
 
-from practicum_7.lu import LinearSystemSolver
+from practicum_9.lu import LinearSystemSolver
 from src.common import NDArrayFloat
 
 
@@ -14,19 +14,66 @@ class LuSolverWithPermute(LinearSystemSolver):
 
     def solve(self, b: NDArrayFloat) -> NDArrayFloat:
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
+        b = np.array(b, dtype=self.dtype)
+        pb = self.P @ b
+        n = self.A.shape[0]
+        y = np.zeros(n, dtype=self.dtype)
 
-        pass
+        for i in range(n):
+            s = self.dtype(0.0)
+
+            for j in range(i):
+                s += self.L[i, j] * y[j]
+
+            y[i] = pb[i] - s
+
+        x = np.zeros(n, dtype=self.dtype)
+
+        for i in range(n - 1, -1, -1):
+            s = self.dtype(0.0)
+
+            for j in range(i + 1, n):
+                s += self.U[i, j] * x[j]
+
+            x[i] = (y[i] - s) / self.U[i, i]
+
+        return x
 
     def _decompose(self, permute: bool) -> tuple[NDArrayFloat, NDArrayFloat, NDArrayFloat]:
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
+        A = np.array(self.A, dtype=self.dtype)
 
-        pass
+        n = A.shape[0]
+
+        U = A.copy()
+        L = np.eye(n, dtype=self.dtype)
+        P = np.eye(n, dtype=self.dtype)
+
+        for k in range(n - 1):
+
+            if permute:
+                max_row = k
+
+                for i in range(k + 1, n):
+                    if abs(U[i, k]) > abs(U[max_row, k]):
+                        max_row = i
+
+                if max_row != k:
+                    U[[k, max_row], :] = U[[max_row, k], :]
+                    P[[k, max_row], :] = P[[max_row, k], :]
+                    if k > 0:
+                        L[[k, max_row], :k] = L[[max_row, k], :k]
+            if U[k, k] == 0:
+                raise ValueError("Zero pivot element")
+            for i in range(k + 1, n):
+                multiplier = U[i, k] / U[k, k]
+
+                L[i, k] = multiplier
+
+                for j in range(k, n):
+                    U[i, j] = U[i, j] - multiplier * U[k, j]
+
+        return L, U, P
 
 
 def get_A_b(a_11: float, b_1: float) -> tuple[NDArrayFloat, NDArrayFloat]:
@@ -41,7 +88,7 @@ if __name__ == "__main__":
     b_1 = -16 + 10 ** (-p)  # add/remove 10**(-p) to check instability
     A, b = get_A_b(a_11, b_1)
 
-    solver = LuSolver(A, np.float64, permute=True)
+    solver = LuSolverWithPermute(A, np.float64, permute=True)
     x = solver.solve(b)
     assert np.all(np.isclose(x, [1, -7, 4])), f"The anwser {x} is not accurate enough"
 
